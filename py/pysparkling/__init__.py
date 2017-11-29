cbf8165b6f608c864ef81d714333e427bfcb6fcf
Jakub Hava <jakub.hava@gmail.com>
2017-11-29 15:18:29 -0800

    [SW-615] pysparkling.__version__ returns incorrectly 'SUBST_PROJECT_VERSION'

--- py/pysparkling/__init__.py
+++ py/pysparkling/__init__.py
@@ -5,7 +5,18 @@
 pySparkling - The Sparkling-Water Python Package
 =====================
 """
-__version__ = "SUBST_PROJECT_VERSION"
+import zipfile
+from codecs import open
+from os import path
+
+here = path.dirname(path.abspath(path.dirname(__file__)))
+if '.zip' in here:
+    with zipfile.ZipFile(here, 'r') as archive:
+        __version__ = archive.read('version.txt')
+else:
+    with open(path.join(here, 'version.txt'), encoding='utf-8') as f:
+        version = f.read()
+
 
 # set imports from this project which will be available when the module is imported
 from pysparkling.context import H2OContext
